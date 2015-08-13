1f1222e71dc49f45e48240e8aefd08b705e0f270
Simon A. Eugster <simon.eugster@eps.ch>
2015-08-13 11:13:09 +0200

    webMI.data.write documentation

--- webMI.d.ts
+++ webMI.d.ts
@@ -11,6 +11,14 @@ declare module AtviseWebMI {
     export interface Data {
         call(name:String, params:Object, fn?:Function);
 
+        /**
+         * Write one or more values to OPC UA nodes.
+         * @param nodes Node address, as string or as array of strings.
+         * @param values Values to write (single value or array of values)
+         * @param {Function} fn Called when the value was written. On success, it is passed an empty object.
+         * On error, it receives an object with an `error` property.
+         * Note that, when writing booleans, numbers != 0 resolve to true.
+         */
         write(nodes:String[], values:any[], fn?:Function);
 
         read(nodeId: String, fn: Function): NodeValue;
